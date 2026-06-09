39bf13af50e2ae08dfebefdf18373ff40b21787a
Gema Bravo Sanchez <gebravo-@c3r4s6.42madrid.com>
2026-06-09 17:55:25 +0200

    v1745_v1_avec_assert

--- data_science_0/ex04/whatis.py
+++ data_science_0/ex04/whatis.py
@@ -3,17 +3,18 @@ import sys
 argv = sys.argv
 argc = len(argv) - 1
 
-if argc == 0:
-    pass
 
-if argc > 1:
-    print("AssertionError: more than one argument is provided")
-elif argc == 1:
-    try:
+try:
+    assert argc <= 1, "more than one argument is provided"
+    if argc == 1:
         number = int(argv[1])
         if number % 2 == 0:
             print("I'm Even.")
         else:
             print("I'm Odd.")
-    except ValueError:
-        print("AssertionError: argument is not an integer")
+
+except AssertionError as error:
+    print(f"AssertionError: {error}")
+
+except ValueError:
+    print("AssertionError: argument is not an integer")
