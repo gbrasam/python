b7021a29aa6e5f43ad47908fe427a09b54d52283
Gema Bravo Sanchez <gebravo-@c2r4s6.42madrid.com>
2026-06-17 11:43:48 +0200

    170626_1143

--- data_science_1_Array/ex00/tester.py
+++ data_science_1_Array/ex00/tester.py
@@ -1,6 +1,12 @@
 from give_bmi import give_bmi, apply_limit
 height = [2.71, 1.15]
 weight = [165.3, 38.4]
+# height = [2.71]
+# weight = [165.3, 38.4]
+# height = [2.71, 1.15]
+# weight = ["Hello", 38.4]
+# height = [2.71, 1.15]
+# weight = []
 bmi = give_bmi(height, weight)
 print(bmi, type(bmi))
 print(apply_limit(bmi, 26))
