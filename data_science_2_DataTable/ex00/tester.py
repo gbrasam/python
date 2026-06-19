cf5c0fd054966321967f3371c488e853cf2cfbbc
Gema Bravo Sanchez <gebravo-@c2r10s6.42madrid.com>
2026-06-19 11:55:31 +0200

    190626_1155

--- data_science_2_DataTable/ex00/tester.py
+++ data_science_2_DataTable/ex00/tester.py
@@ -1,11 +1,7 @@
 from load_csv import load
 
 print(load("../life_expectancy_years.csv"))
-# Error: empty path
 print(load(""))
-# Error: invalid file format
 print(load("../file.txt"))
-# File error: [Errno 2] No such file or directory: '../missing.csv'
 print(load("../missing.csv"))
-# 
 print(load("."))
